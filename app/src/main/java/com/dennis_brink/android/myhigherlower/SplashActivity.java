3958eff4aceeaa223e3f0dda3c21a712a2d6eb3d
Dennis Brink <dennis.mjknd@gmail.com>
2022-06-18 01:35:26 +0200

    Improved splashscreen (fullscreen). Minor other changes (name)

--- app/src/main/java/com/dennis_brink/android/myhigherlower/SplashActivity.java
+++ app/src/main/java/com/dennis_brink/android/myhigherlower/SplashActivity.java
@@ -1,10 +1,19 @@
 package com.dennis_brink.android.myhigherlower;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -19,7 +28,12 @@ public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
 
         textView = findViewById(R.id.textView);
@@ -49,4 +63,5 @@ public class SplashActivity extends AppCompatActivity {
         }.start();
 
     }
+
 }
